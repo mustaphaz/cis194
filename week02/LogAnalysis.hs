36b13429abbd99e1173a28678cbc66b37ae76e1b
Mustapha <mzeamari@bol.com>
2024-10-21 16:56:26 +0200

    (wk02): complete exercise 2

--- week02/LogAnalysis.hs
+++ week02/LogAnalysis.hs
@@ -22,8 +22,14 @@ getTimestamp w
     | w!!0 == "E" = read (w!!2) :: TimeStamp
     | otherwise = read (w!!1) :: TimeStamp
 
-
 getLogText :: [String] -> String
 getLogText w
     | w!!0 == "E" = unwords(drop 3 w)
     | otherwise =  unwords (drop 2 w)
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) t = t
+insert x Leaf = Node Leaf x Leaf
+insert x@(LogMessage _ t1 _) t@(Node lt y@(LogMessage _ t2 _) rt)
+    | t1 < t2 = Node (insert x lt) y rt
+    | otherwise = Node lt y (insert x rt)
